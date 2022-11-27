edc469f2a8a0b110a04f1af589fb688bc149e6e8
Rafael Santos <rafa.05125@gmail.com>
2022-11-27 15:54:34 -0300

    Delete should delete object when id exists test

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.entities.repositories.ProductRepository;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		
+		Long existingId = 1L;
+		
+		repository.deleteById(existingId);
+		
+		Optional<Product> result = repository.findById(existingId);
+		
+		Assertions.assertFalse(result.isPresent());
+	}
+}
