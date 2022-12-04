c1dac49c56f53cb55c83b1f38fbcd9fceaa01988
Rafael Santos <rafa.05125@gmail.com>
2022-12-04 13:12:01 -0300

    ProductResource delete method tested

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -73,6 +74,27 @@ public class ProductResourceTests {
 		doThrow(DatabaseException.class).when(service).delete(dependentId);
 	}
 	
+	
+	@Test
+	public void deleteShouldReturnNoContentWhenIdExists() throws Exception{
+		ResultActions result = 
+				mockMvc.perform(delete("/products/{id}", existingId)
+					.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNoContent());
+	}
+	
+	
+	@Test
+	public void deleteShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+		
+		
+		ResultActions result = 
+				mockMvc.perform(delete("/products/{id}", nonExistingId)
+					.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+	}
 	
 	@Test
 	public void insertShouldReturnCreatedAndProductDTO() throws Exception {
